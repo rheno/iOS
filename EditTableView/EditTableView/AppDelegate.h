ab641265026658802c62e4e2245997c37a12d7b0
rhenobudiasa <rhenobudiasa@yahoo.com>
2013-05-03 00:39:05 +0700

    Commit All

---
+++ EditTableView/EditTableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  EditTableView
+//
+//  Created by Rheno Budiasa on 4/8/13.
+//  Copyright (c) 2013 rheno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainTableView;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainTableView *mainTableView;
+
+@end
---
+++ EditTableView/EditTableView/MainTableView.h
@@ -0,0 +1,13 @@
+//
+//  MainTableView.h
+//  EditTableView
+//
+//  Created by Rheno Budiasa on 4/8/13.
+//  Copyright (c) 2013 rheno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableView : UITableViewController
+
+@end
